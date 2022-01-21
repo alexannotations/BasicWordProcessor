df34be3879c14307082754891a69ff245ecf0479
alexayca <69085537+alexannotations@users.noreply.github.com>
2022-01-21 00:27:47 -0600

    101 . Barra de menus

--- src/MenuFrame.java
+++ src/MenuFrame.java
@@ -8,8 +8,8 @@ public class MenuFrame extends JFrame {
     public MenuFrame() throws HeadlessException {
         setBounds(550,300,850,350);
         setTitle("Procesador de texto basico");
-        setVisible(true);
         add(new MenuPanel());
+        setVisible(true);
     }
 
 }
--- src/MenuPanel.java
+++ src/MenuPanel.java
@@ -1,10 +1,41 @@
 //package src;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MenuPanel extends JPanel {
 
     public MenuPanel() {
+        setLayout(new BorderLayout());
+
+        JMenu fontType = new JMenu("Fuente");
+        JMenu fontStyle = new JMenu("Estilo");
+        JMenu fontSize = new JMenu("Tamaño");
+
+        JMenuBar barraMenuSuperior = new JMenuBar();
+        barraMenuSuperior.add(fontType);
+        barraMenuSuperior.add(fontStyle);
+        barraMenuSuperior.add(fontSize);
+
+        JMenuItem arial = new JMenuItem("Arial");
+        JMenuItem courier = new JMenuItem("Courier");
+        JMenuItem verdana = new JMenuItem("Verdana");
+
+        fontType.add(arial);
+        fontType.add(courier);
+        fontType.add(verdana);
+
+        fontStyle.add(new JMenuItem("Negrita"));
+        fontStyle.add(new JMenuItem("Cursiva"));
+
+        fontSize.add(new JMenuItem("12"));
+        fontSize.add(new JMenuItem("16"));
+        fontSize.add(new JMenuItem("20"));
+        fontSize.add(new JMenuItem("24"));
+
+        JPanel laminaMenu = new JPanel();
+        laminaMenu.add(barraMenuSuperior);
+        add(laminaMenu, BorderLayout.NORTH);
 
     }
 }
