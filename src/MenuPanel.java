abc7f1b16d4fe8326d90c7e1b9d46d82541e1530
alexayca <69085537+alexannotations@users.noreply.github.com>
2022-01-21 01:53:19 -0600

    103 . Nueva barra de menus generada con un metodo

--- src/MenuPanel.java
+++ src/MenuPanel.java
@@ -28,11 +28,32 @@ public class MenuPanel extends JPanel {
         barraMenuSuperior.add(fontType);
         barraMenuSuperior.add(fontStyle);
         barraMenuSuperior.add(fontSize);
+        /* -- -- -- -- -- -- -- -- -- -- -- -- TODO refactorizar-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --*/
+        configurarMenu("Arial","Fuente","",0,0);
+        configurarMenu("Consolas","Fuente","",0,0);
+        configurarMenu("Verdana","Fuente","",0,0);
+
+        configurarMenu("Negrita","Estilo","",0,0);
+        configurarMenu("Cursiva","Estilo","",0,0);
+
+        configurarMenu("12","Tamaño","",0,0);
+        configurarMenu("16","Tamaño","",0,0);
+        configurarMenu("20","Tamaño","",0,0);
+        configurarMenu("24","Tamaño","",0,0);
 
     }
 
-    public void configurarMenu(String rotulo, String menu, String tipoLetra, int estilo, int sizeLetra){
-
+    /** Un metodo que ponga los elementos a la escucha y que tamabien los construya */
+    public void configurarMenu(String rotuloMenuSuperior, String jmenuSup, String tipoLetra, int estiloLetra, int sizeLetra){
+        // Construye los items para cada submenu
+        JMenuItem menuElement = new JMenuItem(rotuloMenuSuperior);
+        if (jmenuSup=="Fuente"){
+            fontType.add(menuElement);
+        }else if(jmenuSup=="Estilo"){
+            fontStyle.add(menuElement);
+        }else if(jmenuSup=="Tamaño"){
+            fontSize.add(menuElement);
+        }
     }
 
 }
