1cdcd762ddcd8c22b4f44d72ac89924b489b32ab
alexayca <69085537+alexannotations@users.noreply.github.com>
2022-01-21 04:03:42 -0600

    106 . Implementado StyledEditorKit para manipular texto en JTextPane

--- src/MenuPanel.java
+++ src/MenuPanel.java
@@ -1,6 +1,7 @@
 //package src;
 
 import javax.swing.*;
+import javax.swing.text.StyledEditorKit;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -27,7 +28,7 @@ public class MenuPanel extends JPanel {
         add(areaInfo, BorderLayout.SOUTH);
 
         panelMenuSuperior.add(barraMenuSuperior);
-        areaInfo.add(new JLabel("Tipo: " + "tipoLetra" +". Estilo: " + "style" +". Tamaño: " + "size"));
+        areaInfo.add(new JLabel("Ejemplo de info ..."));
         barraMenuSuperior.add(fontType);
         barraMenuSuperior.add(fontStyle);
         barraMenuSuperior.add(fontSize);
@@ -38,7 +39,6 @@ public class MenuPanel extends JPanel {
 
         configurarMenu("Negrita","Estilo","",Font.BOLD,1);
         configurarMenu("Cursiva","Estilo","",Font.ITALIC,1);
-        configurarMenu("Normal","Estilo","",Font.PLAIN,1);
 
         configurarMenu("12","Tamaño","",9,12);
         configurarMenu("16","Tamaño","",9,16);
@@ -53,57 +53,29 @@ public class MenuPanel extends JPanel {
         JMenuItem menuElement = new JMenuItem(rotuloMenuSuperior);
         if (jbotonMenuSup=="Fuente"){
             fontType.add(menuElement);
-        }else if(jbotonMenuSup=="Estilo"){
-            fontStyle.add(menuElement);
-        }else if(jbotonMenuSup=="Tamaño"){
-            fontSize.add(menuElement);
-        }
-        // Ponemos a la escucha al menu desplegable
-        menuElement.addActionListener(new GestionaEventos(rotuloMenuSuperior, jbotonMenuSup, estiloLetra, sizeLetra));
-
-    }
-
-    // Clase interna
-    private class GestionaEventos implements ActionListener{
-
-        private String tipoLetra, menuSelecionado;
-        private int style, size;
-
-        // Constructor que detectara que item del menu se selecciono
-        GestionaEventos(String elementoSeleccionado, String botonFuente, int estiloLetra, int tamLetra) {
-            tipoLetra = elementoSeleccionado;
-            menuSelecionado = botonFuente;
-            style = estiloLetra;
-            size = tamLetra;
-
-        }
-
-        /** Cambia el tipo de letra, estilo y tamaño */
-        @Override
-        public void actionPerformed(ActionEvent e) {
-
-            letras = areaEscritura.getFont();       // Captura el tipo de letra del area de texto
-
-            if (menuSelecionado =="Arial" || menuSelecionado =="Consolas" || menuSelecionado =="Verdana"){
-                style = letras.getStyle();
-                size = letras.getSize();
-            } else
-            if (menuSelecionado =="Negrita" || menuSelecionado =="Cursiva" || menuSelecionado =="Normal"){
-                if (letras.getStyle()==0 || letras.getStyle()==1 || letras.getStyle()==2 || letras.getStyle()==3 ){
-                    // TODO: Checar si se conserva las negritas y cursivas
-                }
-                tipoLetra = letras.getFontName();
-                size = letras.getSize();
-            }else
-            if (menuSelecionado =="12" || menuSelecionado =="16" || menuSelecionado =="20" || menuSelecionado =="24"){
-                style = letras.getStyle();
-                tipoLetra = letras.getFontName();
+            if (tipoLetra=="Arial"){
+                menuElement.addActionListener(new StyledEditorKit.FontFamilyAction("Cambia letra", "Arial"));
+            }else if (tipoLetra=="Consolas"){
+                menuElement.addActionListener(new StyledEditorKit.FontFamilyAction("Cambia letra", "Consolas"));
+            }else if(tipoLetra=="Verdana"){
+                menuElement.addActionListener(new StyledEditorKit.FontFamilyAction("Cambia letra", "Verdana"));
             }
 
-            areaEscritura.setFont(new Font(tipoLetra, style, size));
-            System.out.println("Tipo: " + tipoLetra +" Estilo: " + style +" Tamaño: " + size);  // TODO: Agregar a la areaInfo
+        }else if(jbotonMenuSup=="Estilo"){
+            fontStyle.add(menuElement);
+            if (estiloLetra==Font.BOLD){
+                menuElement.addActionListener(new StyledEditorKit.BoldAction());
+            } else if (estiloLetra==Font.ITALIC){
+                menuElement.addActionListener(new StyledEditorKit.ItalicAction());
+            }
+
+        }else if(jbotonMenuSup=="Tamaño"){
+            fontSize.add(menuElement);
+            menuElement.addActionListener(new StyledEditorKit.FontSizeAction("Cambia tamañ",sizeLetra));
         }
 
     }
+
+
 
 }
